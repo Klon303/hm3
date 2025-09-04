83692dc371a1656a39662c45bd50511d1a2506f2
Igor_Maer <igor.son303@yandex.by>
2025-09-04 22:43:55 +0300

    hw5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,32 +1,40 @@
-import { Routes, Route, Navigate, NavLink } from "react-router-dom";
-import Error404 from "./pages/Error404";
-import PreJunior from "./pages/PreJunior";
-import Junior from "./pages/Junior";
-import JuniorPlus from "./pages/JuniorPlus";
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Error404 from './pages/Error404'
+import PreJunior from './pages/PreJunior'
+import Junior from './pages/Junior'
+import JuniorPlus from './pages/JuniorPlus'
 
 export const PATH = {
-  PRE_JUNIOR: "/pre-junior",
-  JUNIOR: "/junior",
-  JUNIOR_PLUS: "/junior-plus",
-};
-
-function Pages() {
-  return (
-    <div>
-      <Routes>
-        {/*роутинг будут писать студенты*/}
-        {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-
-        <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR} />} />
-        {/*роуты для /pre-junior, /junior, /junior-plus*/}
-        <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
-        <Route path={PATH.JUNIOR} element={<Junior />} />
-        <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
-        {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-        <Route path="/*" element={<Error404 />} />
-      </Routes>
-    </div>
-  );
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus',
 }
 
-export default Pages;
+function Pages() {
+    return (
+        <div>
+            {/*Routes выбирает первый подходящий роут*/}
+            <Routes>
+                {/*роутинг будут писать студенты*/}
+                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+                <Route path={'/'} element={<PreJunior/>}/>
+
+                {/*роуты для /pre-junior, /junior, /junior-plus*/}
+                <Route path='/pre-junior' element={<PreJunior/>}/>
+                <Route path='/junior' element={<Junior/>}/>
+                <Route path='/junior-plus' element={<JuniorPlus/>}/>
+                {/*<Route ...*/}
+                {/*<Route ...*/}
+                {/*<Route ...*/}
+
+                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                {/*<Route ...*/}
+                <Route path='/error404' element={<Error404/>}/>
+                <Route path='/*' element={<Navigate to={'/error404'}/>}/>
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
